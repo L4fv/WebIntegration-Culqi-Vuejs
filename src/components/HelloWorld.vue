06ce35a81f4a4a64b4d732216b2ccb9b2f867a77
LUIS FLORES <luanflov92@gmail.com>
2019-12-20 15:49:14 -0500

    RC 0.1

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,120 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <form>
+      <div>
+        <h5>ORDEN: {{order_id}}</h5>
+      </div>
+      <div>
+        <h5>PRECIO: {{currency_code}} {{amount}}</h5>
+      </div>
+      <div>
+        <h5>DETALLE: {{description}}</h5>
+      </div>
+      <div>
+        <label>
+          <span>Correo Electrónico</span>
+          <input type="text" size="50" data-culqi="card[email]" id="card[email]" v-model="email" />
+        </label>
+      </div>
+      <div>
+        <label>
+          <span>Número de tarjeta</span>
+          <input
+            type="text"
+            size="20"
+            data-culqi="card[number]"
+            id="card[number]"
+            v-model="creditcard"
+          />
+        </label>
+      </div>
+      <div>
+        <label>
+          <span>CVV</span>
+          <input type="text" size="4" data-culqi="card[cvv]" id="card[cvv]" v-model="cvv" />
+        </label>
+      </div>
+      <div>
+        <label>
+          <span>Fecha expiración (MM/YYYY)</span>
+          <input size="2" data-culqi="card[exp_month]" id="card[exp_month]" v-model="month" />
+          <span>/</span>
+          <input size="4" data-culqi="card[exp_year]" id="card[exp_year]" v-model="year" />
+        </label>
+      </div>
+      <div>
+        <input type="number" size="4" v-model="installments" />
+      </div>
+    </form>
+    <button id="btn_pagar" @click="payment">Pagar</button>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String,
+
+  data() {
+    return {
+      order_id: 123,
+      description: 'PRODUCTO ABC',
+      amount: 1200,
+      installments: 2,
+      telefono: 986193329,
+      currency_code: 'PEN',
+      email: 'hola@trivelapp.com',
+      /* data sensible */
+      creditcard: '4111 1111 1111 1111',
+      month: '09',
+      year: '2020',
+      cvv: '123',
+    };
+  },
+  methods: {
+    async getToken() {
+      return new Promise((resolve) => {
+        let c = 0;
+        _Culqi.createToken();
+        const checkToken = setInterval(() => {
+          c++;
+          if (c > 20) clearInterval(checkToken);
+          if (_Culqi.token) {
+            clearInterval(checkToken);
+            console.log('token en variable', _Culqi.token);
+            resolve(_Culqi.token);
+          } else {
+          }
+        }, 1000);
+      });
+    },
+    async payment() {
+      const resultado = await this.getToken();
+      if (resultado) {
+        // ¡Objeto Token creado exitosamente!
+        const token = resultado.id;
+        const response = await axios({
+          method: 'post',
+          url: '/api/process/pay', // YOUR BACKEND
+          data: {
+            order_id: this.order_id,
+            description: this.description,
+            amount: this.amount,
+            installments: this.installments,
+            telefono: this.telefono,
+            currency_code: this.currency_code,
+            token,
+          },
+          headers: {
+            'content-type': 'application/json',
+          },
+        });
+        console.log(response.data);
+      } else {
+        console.log(_Culqi.error);
+      }
+    },
   },
 };
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
